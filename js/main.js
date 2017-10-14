e39f7ec9a513ef0eee3d99e0bca2d3eeb9111713
Steven Holmes <steven.w.holmes@gmail.com>
2017-10-14 17:10:48 -0500

    audio player

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+let play = $('.play'),
+    pause = $('.pause'),
+    runaway = new Howl({
+        src: ['assets/run_away.mp3']
+    });
+
+const wave = new SiriWave({
+        container: document.getElementById('wave'),
+        width: 400,
+        height: 30,        
+        speed: 0.05,
+        color: '#cecece',
+        frequency: 10,
+        autostart: false,
+        amplitude: .5,
+        amplitudeInterpolationSpeed: 100
+});
+
 $('.resource').on('mouseover', (e) => {
     $(e.target).removeClass('hover-off');
     $(e.target).addClass('hover-on');
@@ -8,7 +26,24 @@ $('.resource').on('mouseleave', (e) => {
     $(e.target).removeClass('hover-on');
 });
 
-$('.play').on('click', (e) => {
-    console.log("Play song");
-    $(e.target).html('pause');
+$(play).on('click', () => {
+    console.log(wave);
+    wave.start();
+    play.hide();
+    pause.show();
+    runaway.play();
+
 });
+
+$(pause).on('click', () => {
+    console.log('click');
+    pause.hide();
+    play.show();
+    runaway.pause();
+    wave.stop();
+});
+
+
+
+
+
